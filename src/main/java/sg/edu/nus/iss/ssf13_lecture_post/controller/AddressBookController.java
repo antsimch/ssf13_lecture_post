86bdd4048dcd506680b23f3322da876bf9a22d91
antsimch <anthonysimch@gmail.com>
2023-05-17 12:22:21 +0800

    ObjectError

--- src/main/java/sg/edu/nus/iss/ssf13_lecture_post/controller/AddressBookController.java
+++ src/main/java/sg/edu/nus/iss/ssf13_lecture_post/controller/AddressBookController.java
@@ -1,10 +1,12 @@
 package sg.edu.nus.iss.ssf13_lecture_post.controller;
 
 import org.apache.coyote.http11.filters.GzipOutputFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,10 +15,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.validation.Valid;
 import sg.edu.nus.iss.ssf13_lecture_post.model.Contact;
+import sg.edu.nus.iss.ssf13_lecture_post.utility.Utility;
 
 @Controller
 @RequestMapping (path = "/")
 public class AddressBookController {
+
+    @Autowired
+    Utility utility;
     
     @GetMapping
     public String getAddressBook(Model model) {
@@ -54,6 +60,12 @@ public class AddressBookController {
             // System.out.println("No. of errors: " + bindingResult.getErrorCount());
             // String errorMessage = "Error in validating attribute";
             // model.addAttribute("errorMessage", errorMessage);
+            return "addressBook";
+        }
+
+        if (!utility.isUniqueEmail(contact.getEmail())) {
+            ObjectError error = new ObjectError("globalError", "%s is not available".formatted(contact.getEmail()));
+            bindingResult.addError(error);
             return "addressBook";
         }
 
---
+++ src/main/java/sg/edu/nus/iss/ssf13_lecture_post/utility/Utility.java
@@ -0,0 +1,14 @@
+package sg.edu.nus.iss.ssf13_lecture_post.utility;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Utility {
+    
+    public boolean isUniqueEmail(String email) {
+
+        
+
+        return false;
+    }
+}
