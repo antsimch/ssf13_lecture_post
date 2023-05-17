a7c91b113379cc0de0e8bebe8f8d09ebaa2e9c84
antsimch <anthonysimch@gmail.com>
2023-05-17 11:51:30 +0800

    add validation

--- src/main/java/sg/edu/nus/iss/ssf13_lecture_post/controller/AddressBookController.java
+++ src/main/java/sg/edu/nus/iss/ssf13_lecture_post/controller/AddressBookController.java
@@ -1,11 +1,17 @@
 package sg.edu.nus.iss.ssf13_lecture_post.controller;
 
+import org.apache.coyote.http11.filters.GzipOutputFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.validation.Valid;
 import sg.edu.nus.iss.ssf13_lecture_post.model.Contact;
 
 @Controller
@@ -18,11 +24,39 @@ public class AddressBookController {
         return "addressBook";
     }
 
-    @PostMapping("/contact")
-    public String saveAddressBook(Contact contact, Model model) {
-        System.out.println(contact.getName());
-        System.out.println(contact.getEmail());
-        System.out.println(contact.getTelNo());
+    // @PostMapping(consumes = "application/x-www-form-urlencoded", path = "/contact")
+    // public String saveAddressBook(Contact contact) {
+    //     System.out.println("Name: " + contact.getName());
+    //     System.out.println("Email: " + contact.getEmail());
+    //     System.out.println("Tel No.: " + contact.getTelNo());
+    //     return "addressBook";
+    // }
+
+    // @PostMapping(consumes = "application/x-www-form-urlencoded", path = "/contact")
+    // public String saveAddressBook(@RequestBody MultiValueMap<String, String> form, Model model) {
+
+    //     String name = form.getFirst("name");
+    //     String email = form.getFirst("email");
+    //     String telNo = form.getFirst("telNo");
+
+    //     System.out.println("using MultiValueMap: " + name);
+        
+    //     return "addressBook";
+    // }
+
+    @PostMapping(consumes = "application/x-www-form-urlencoded", path = "/contact")
+    public String saveAddressBook(@Valid Contact contact, BindingResult bindingResult, Model model) {
+        System.out.println("Name: " + contact.getName());
+        System.out.println("Email: " + contact.getEmail());
+        System.out.println("Tel No.: " + contact.getTelNo());
+
+        if (bindingResult.hasErrors()) {
+            // System.out.println("No. of errors: " + bindingResult.getErrorCount());
+            // String errorMessage = "Error in validating attribute";
+            // model.addAttribute("errorMessage", errorMessage);
+            return "addressBook";
+        }
+
         return "addressBook";
     }
 
--- src/main/java/sg/edu/nus/iss/ssf13_lecture_post/model/Contact.java
+++ src/main/java/sg/edu/nus/iss/ssf13_lecture_post/model/Contact.java
@@ -1,8 +1,21 @@
 package sg.edu.nus.iss.ssf13_lecture_post.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class Contact {
+
+    @NotNull(message = "Name cannot be empty")
+    @Size(min = 3, max = 15, message = "Name should be between 3 to 15 characters")
     private String name;
+
+    @NotEmpty(message = "Email cannot be empty")
+    @Email(message = "Invalid email")
     private String email;
+
+    @Size(min = 8, message = "Invalid phone number")
     private String telNo;
 
     public Contact() {
