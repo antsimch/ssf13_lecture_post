7d930bb07c60a941814a38fd52eb947888fd2695
antsimch <anthonysimch@gmail.com>
2023-05-17 09:22:47 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/ssf13_lecture_post/Ssf13LecturePostApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.ssf13_lecture_post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssf13LecturePostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ssf13LecturePostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/ssf13_lecture_post/Ssf13LecturePostApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.ssf13_lecture_post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssf13LecturePostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
